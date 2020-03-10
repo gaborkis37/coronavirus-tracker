f730014b4136e9f020b27822ed7d6c5c1ce251f6
Gabor Kis <gabor_kis@epam.com>
2020-03-10 12:10:38 +0100

    bootstrap added to the page to look nicer

--- src/main/java/com/virustracker/controller/HomeController.java
+++ src/main/java/com/virustracker/controller/HomeController.java
@@ -1,22 +1,29 @@
 package com.virustracker.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.virustracker.model.LocationStats;
 import com.virustracker.service.VirusDataService;
 
 @Controller
 public class HomeController {
-	
+
 	@Autowired
 	VirusDataService dataService;
 
 	@GetMapping("/")
-	public String home(Model model ) {
-		model.addAttribute("locationStats", dataService.getAllStats());
+	public String home(Model model) {
+		List<LocationStats> allStats = dataService.getAllStats();
+		int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+		model.addAttribute("locationStats", allStats);
+		model.addAttribute("totalReportedCases", totalReportedCases);
+
 		return "home";
 	}
-	
+
 }
